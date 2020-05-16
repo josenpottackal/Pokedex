41272350660e2796132d46b0aa6a012c63faf38f
Josen Pottackal <60514271+josenpottackal@users.noreply.github.com>
2020-05-16 11:14:36 +0100

    Add files via upload
    JavaScript file responsible for fetching and displaying data

---
+++ Compare.js
@@ -0,0 +1,73 @@
+/*
+  File Name: Compare.js
+  Purpose: Fetches and displays Pokemon data
+  Date: 2020-05-14
+  Last modified: 2020-05-14
+  Author: Josen Pottackal
+  Copy right no copyright
+  Version: 1.0
+*/
+
+const pokedex = document.getElementById('pokedex');
+
+/* Fetches Pokemon data */
+const fetchPokemon = async () => {
+    const url = `https://pokeapi.co/api/v2/pokemon/`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = data.results.map((result, index) =>
+    ({
+        ...result,
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+    }));
+    displayPokemon(pokemon);
+};
+
+/* Displays Pokemon data */
+const displayPokemon = (pokemon) => {
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card" onclick="selectPokemon(${pokeman.id})">
+            <img class="cardImage" src="${pokeman.image}"/>
+            <h2 class="cardTitle">${pokeman.name}</h2>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+/* Displays selcted Pokemon */
+const selectPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokeman = await res.json();
+    displayPopup(pokeman);
+};
+
+/* Displays Popup */ 
+const displayPopup = (pokeman) => {
+    const type = pokeman.types.map((type) => type.type.name).join(', ');
+    const image = pokeman.sprites['front_default'];
+    const htmlString =
+   `<div class="compareCard">
+        <button class="button button2" onclick="closePopup();">Remove</button>
+        <div class="card">
+            <h2 class="compareTitle">Compare:</h2>
+            <img class="cardImage" src="${image}"/>
+            <h2 class="cardTitle">${pokeman.name}</h2>
+            <p><small>Height: </small>${pokeman.height} | <small>Weight: </small>${pokeman.weight} | <small>Type: </small>${type}
+        </div>
+    </div>`;
+    compare.innerHTML = htmlString + compare.innerHTML;
+};
+
+/* Closes Popup */
+const closePopup = () => {
+    const compareCard = document.querySelector('.compareCard');
+    compareCard.parentElement.removeChild(compareCard);
+};
+
+fetchPokemon();
